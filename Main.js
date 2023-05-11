1ec9ef4bd8e036bb559409a908b76f6dae0bbc44
Sayf <Sayf.Persevi@outlook.com>
2023-05-11 23:03:12 +0200

    Fixed css

--- Main.js
+++ Main.js
@@ -6,22 +6,26 @@ let users = []
 
 searchInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase()
+  let x = 0
   users.forEach(user => {
     const isVisable = user.title.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisable)
   })
 })
 
-fetch("https://www.omdbapi.com/?apikey=a0a957d2&s=fast")
+
+  fetch("https://www.omdbapi.com/?apikey=a0a957d2&s=fast")
   .then(res => res.json())
-    .then(movieData => {
-      users = movieData.Search.map(data => {
-        const movie = movieCardTemplate.content.cloneNode(true).children[0]
-        const title = movie.querySelector("[data-title]")
-        const poster = movie.getElementsByClassName("movie__poster")
-        title.textContent = data.Title
-        poster[0].src = data.Poster
+  .then(movieData => {
+    users = movieData.Search.map(data => {
+      const movie = movieCardTemplate.content.cloneNode(true).children[0]
+      const title = movie.querySelector("[data-title]")
+      const poster = movie.getElementsByClassName("movie__poster")
+      title.textContent = data.Title
+      poster[0].src = data.Poster
+
         movieCardContainer.append(movie)
-        return {title: data.Title, element: movie}
-      })
-    })  
+      
+      return {title: data.Title, element: movie}
+    })
+  })  
